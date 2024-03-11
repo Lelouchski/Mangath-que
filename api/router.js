ce4374d30c8022fa624a22872940f3b93969fca0
Lelouchski <valentin.sepre@gmail.com>
2024-03-11 16:47:08 +0100

    end

--- api/router.js
+++ api/router.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const homeController = require('./Controllers/HomeController')
+const userController = require('./Controllers/UserController')
 
 router.route('/').get(homeController.get)
 
+router.route('/Account').get(userController.get)
 
 module.exports = router
 
